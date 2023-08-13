cad7e385cb7fdcc93bb768c024b31d47b40f6ca6
Akshay2002Singh <06akshay2002@gmail.com>
2023-08-13 21:10:28 +0530

    added backend hosted url

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,5 +1,6 @@
 const url = {
-    backend_url : "http://localhost:3000"
+    backend_url : "https://snapswapbackend.onrender.com/"
+    // backend_url : "http://localhost:3000"
 }
 
 export default url
